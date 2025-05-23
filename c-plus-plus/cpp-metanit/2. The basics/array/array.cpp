6db9a5853385faebb13e51d22a9872e3de3a388e
Darya Strezh <63409744+DaryaStrezh@users.noreply.github.com>
2025-05-23 15:39:43 +0300

    Update array.cpp

--- c-plus-plus/cpp-metanit/2. The basics/array/array.cpp
+++ c-plus-plus/cpp-metanit/2. The basics/array/array.cpp
@@ -3,3 +3,18 @@
 * Пользователь должен вводить с консоли значения для всех элементов массива. 
 * После завершения ввода всех чисел программа должна вывести элементы массива в обратном порядке.
 */
+#include <iostream>
+
+int main() {
+	const int array_size{ 10 };
+	int num{ 0 };
+	int nums[array_size];
+	for (int i = 0; i < array_size; i++) {
+		std::cout << "enter " << i + 1 << " element: ";
+		std::cin >> num;
+		nums[i] = num;
+	}
+	for (int i = array_size - 1; i >= 0; i--) {
+		std::cout << nums[i] << " ";
+	}
+}
